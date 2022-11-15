40bfbcf8bc6bc6f09036c817133702aef095c103
RaphaelC1 <100593405+RaphaelC1@users.noreply.github.com>
2022-11-15 12:09:43 +0100

    Menu principal pas encore fini

--- main.c
+++ main.c
@@ -2,19 +2,10 @@
 #include <stdlib.h>
 #include "display/displayTxt.h"
 #include "three.h"
+#include "menu.h"
 int main() {
-    //three *ThreeForAllLetter=createInitialThree(); //Creer un three avec un tableau de toutes les lettres en root
-    //listString test = getBasesFormes("D:\\generateurdemot\\ProjetCGenerateurDePhraseV2\\dictionnaire.txt");
-    //displayStringList(test);
-    listString test = malloc(sizeof (slistString));
-    test->head=NULL;
-    test= getBasesFormesNom("D:/generateurdemot/ProjetCGenerateurDePhraseV2/test.txt");
-    //displayStringList(test);
-    //displayStringList(test);
-    three tree=createInitialThree();
-    addBasesFormesInTree(test,&tree);
-    printf("three root = %c\n"
-           //"three sec = %c",
-           ,tree.root[18]->children->head->data->data);
+    startMenu();
+    controlStartMenu();
+
     return 0;
 }
---
+++ menu.h
@@ -0,0 +1,18 @@
+//
+// Created by raphou on 14/11/2022.
+//
+#include <stdio.h>
+#include <windows.h>
+#ifndef PROJETCGENERATEURDEPHRASEV2_MENU_H
+#define PROJETCGENERATEURDEPHRASEV2_MENU_H
+
+void startMenu();
+void Color(int couleurDuTexte,int couleurDeFond);
+void controlStartMenu();
+int controlInput(int min,int max);
+
+
+
+
+
+#endif //PROJETCGENERATEURDEPHRASEV2_MENU_H
