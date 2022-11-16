02e0be4a5d70b6a833c54d64c38f0d0e98e3723e
RaphaelC1 <100593405+RaphaelC1@users.noreply.github.com>
2022-11-16 18:28:06 +0100

    tout beau tout propre

--- menu/menu.h
+++ menu/menu.h
@@ -3,12 +3,18 @@
 //
 #include <stdio.h>
 #include <windows.h>
+#include "list.h"
+#include "display/displayTxt.h"
 #ifndef PROJETCGENERATEURDEPHRASEV2_MENU_H
 #define PROJETCGENERATEURDEPHRASEV2_MENU_H
 
 void startMenu();
 void Color(int couleurDuTexte,int couleurDeFond);
+void controlGeneratorMenu();
+void GeneratorMenu();
 void controlStartMenu();
+void controlListMenu();
+void listMenu();
 int controlInput(int min,int max);
 
 
