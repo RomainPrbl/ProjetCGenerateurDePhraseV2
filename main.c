e6ab29f226217cd42b40983b192ff101e1d8d3c9
RaphaelC1 <100593405+RaphaelC1@users.noreply.github.com>
2022-11-16 21:21:43 +0100

    Pret pour demain

--- main.c
+++ main.c
@@ -4,9 +4,11 @@
 #include "tree.h"
 #include "menu/menu.h"
 int main() {
-    tree ThreeForAllLetter=createInitialTree(); //Creer un three avec un tableau de toutes les lettres en root
-    listString test = getBasesFormes("../dictionnaire.txt");
-    tree tree=createInitialTree();
-    addBasesFormesInTree(test,&tree);
+    startMenu();
+    controlStartMenu();
+//    tree ThreeForAllLetter=createInitialTree(); //Creer un three avec un tableau de toutes les lettres en root
+//    listString test = getBasesFormes("../dictionnaire.txt");
+//    tree tree=createInitialTree();
+//    addBasesFormesInTree(test,&tree);
         return 0;
 }
